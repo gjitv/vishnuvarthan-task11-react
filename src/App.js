73023f53f6daa154ffa8d225da3ee7b70c05befd
Vishnu <r.vishnuvarthan48@gmail.com>
2023-06-21 15:24:06 +0530

    third

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { SignIn } from "./Components/Login";
 import Main from "./Components/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./Components/Cart";
-import Admin from "./Components/Admin/Admin";
+
 
 const App = () => {
   return (
     <BrowserRouter>  
-      {/* <Routes>
+      <Routes>
         <Route path="/"  element={<Main/>}/>
         <Route path="/signup" element={<SignIn/>}/>  
         <Route path="/cart" element={<Cart/>}/>     
-      </Routes> */}
-      <Admin/>
+      </Routes>
+      
     </BrowserRouter>
   );
 };
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 import {cartData} from "./reducer"
 import { productData } from "./ProductReducer";
-import {addProduct} from "./addProductReducer"
 export default combineReducers({
     cartData,
     productData,
-    addProduct
 })
